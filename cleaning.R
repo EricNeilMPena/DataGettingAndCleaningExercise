29b7a38195268e950a770bad477f0ce3b4f76b59
sc-kuo <48643112+sc-kuo@users.noreply.github.com>
2019-04-15 11:31:59 +0800

    Update cleaning.R

--- cleaning.R
+++ cleaning.R
@@ -1,6 +1,8 @@
 library(RSQLite)
 library(DBI)
 library(dplyr)
+library(lubridate)
+
 #extract data from Northwind DB
 con1 <- dbConnect(RSQLite::SQLite(), dbname="Northwind.sqlite")
 alltables <- dbListTables(con1)
@@ -20,6 +22,8 @@ for (i in 1:length(alltables2)) {
             assign(paste0(tables2[i,]),dbGetQuery(con2,paste0('select * from ',tables2[i,]) ))
       }
 }
+
+#4.2
 #copy customers into customers_dimension and remove missing values
 customers_dimension<-Customers
 for(i in 1:nrow(customers_dimension)){
@@ -34,35 +38,84 @@ customers_dimension<-customers_dimension[order(customers_dimension$CompanyName),
 #create primary key for customers_dimension
 customers_dimension$customer_key<-1:nrow(customers_dimension)
 
+#4.3
 #copy employees into employees_dimension and remove missing values
 employees_dimension<-Employees
 for(i in 1:nrow(employees_dimension)){
-  for(j in 3:ncol(employees_dimension)){
-    if(is.na(employees_dimension[i,j])){
-      employees_dimension[i,j]<-"Missing"
-    }
-  }
+      for(j in 3:ncol(employees_dimension)){
+            if(is.na(employees_dimension[i,j])){
+                  employees_dimension[i,j]<-"Missing"
+            }
+      }
 }
 #sort by LastName
 employees_dimension<-employees_dimension[order(employees_dimension$LastName),]
 #create primary key for employees_dimension
 employees_dimension$employee_key<-1:nrow(employees_dimension)
 
+#4.4
 #suppliers_dimension
 #copy Suppliers into suppliers_dimension and remove missing values
 suppliers_dimension<- Suppliers
 for(i in 1:nrow(suppliers_dimension)){
-  for(j in 3:ncol(suppliers_dimension)){
-    if(is.na(suppliers_dimension[i,j])){
-      suppliers_dimension[i,j]<-"Missing"
-    }
-  }
+      for(j in 3:ncol(suppliers_dimension)){
+            if(is.na(suppliers_dimension[i,j])){
+                  suppliers_dimension[i,j]<-"Missing"
+            }
+      }
 }
 
 #sort by CompanyName
 suppliers_dimension <- suppliers_dimension[order(suppliers_dimension$CompanyName),]
 #create primary key for customers_dimension
 suppliers_dimension$supplier_key<-1:nrow(suppliers_dimension)
+
+
+#4.1
+temp<-as.data.frame(1:3652)
+order_date_dimension<-rbind(order_date_dimension,temp)
+colnames(order_date_dimension)<-"order_date_key"
+order_date_dimension$order_date<-seq(ymd("2010-01-01"),ymd("2019-12-31"),by= "1 day")
+order_date_dimension$order_day_of_week<-weekdays(order_date_dimension$order_date)
+
+for (i in 1:nrow(order_date_dimension)){
+      ifelse(order_date_dimension[i,3]=="Sunday",order_date_dimension$type_of_day[i]<-"Weekend",
+             ifelse(order_date_dimension[i,3]=="Saturday",order_date_dimension$type_of_day[i]<-"Weekend",
+                    order_date_dimension$type_of_day[i]<-"Weekday"))
+}
+order_date_dimension$day<-day(order_date_dimension$order_date)
+order_date_dimension$month<-month(order_date_dimension$order_date,label=TRUE,abbr=FALSE)
+order_date_dimension$year<-year(order_date_dimension$order_date)
+
+for(i in 1:nrow(order_date_dimension)){
+      if(order_date_dimension$month[i]=="February" & leap_year(order_date_dimension$year[i])==TRUE){
+            end<-29
+      }
+      else if (order_date_dimension$month[i]=="February" & leap_year(order_date_dimension$year[i])==FALSE){
+            end<-28
+      }
+      else {
+            end<-30
+      }
+      if(order_date_dimension$day[i]==15|order_date_dimension$day[i]==end){
+            if(order_date_dimension$type_of_day[i]=="Weekday"){
+                  order_date_dimension$payday[i]<-"Yes"
+            }
+            else if(order_date_dimension$type_of_day[i]=="Weekend"){
+                  if (order_date_dimension$type_of_day[i-1]=="Weekday"){
+                        order_date_dimension$payday[i-1]<-"Yes"
+                        order_date_dimension$payday[i]<-"No"
+                  }
+                  else {
+                        order_date_dimension$payday[i-2]<-"Yes"
+                        order_date_dimension$payday[i]<-"No"
+                  }
+            }
+      }
+      else{
+            order_date_dimension$payday[i]<-"No"
+      }
+}
 
 #products_dimension
 #copy Products into Products_dimension and remove missing values
